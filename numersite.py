65347dfba00734fd8002839840c4bf851b8d5d52
jumponpat sakekun <152254050+6604062630111@users.noreply.github.com>
2025-09-16 00:53:24 +0700

    Add files via upload

--- numersite.py
+++ numersite.py
@@ -136,5 +136,9 @@ def index():
     return render_template_string(HTML, summary=summary)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
